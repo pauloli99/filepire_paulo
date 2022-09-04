6ba0f09ae23cb13392ccd866134a5ed61d25ca11
pauloli <616752336@qq.com>
2022-09-05 02:57:04 +0800

    feat: redux setup

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
 
-const Movies = () => (
-  <div>Movies</div>
-);
+import { useGetMoviesQuery } from '../../services/TMDB';
+
+const Movies = () => {
+  const { data } = useGetMoviesQuery();
+
+  console.log(data);
+
+  return (
+
+    <div>Movies</div>
+  );
+};
 
 export default Movies;
