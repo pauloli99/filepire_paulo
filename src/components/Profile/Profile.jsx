9e8f6f2eaaa129c15a8cc02ffa65897ab6062c90
李昌松 <616752336@qq.com>
2022-09-13 18:55:45 +0800

    feat: Authentication Solution

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Profile = () => (
-  <div>Profile</div>
-);
+import { userSelector } from '../../features/auth';
+
+const Profile = () => {
+  const { user } = useSelector(userSelector);
+
+  console.log(user);
+
+  return (
+    <div>Profile - {user.username}</div>
+  );
+};
 
 export default Profile;
