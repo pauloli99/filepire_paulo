b092472b1a2dbd1bffca060564a7fc459b8ebf49
李昌松 <616752336@qq.com>
2022-09-14 18:25:35 +0800

    fix: Minor Fixes

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -78,7 +78,7 @@ const NavBar = () => {
                 <Avatar
                   style={{ width: 30, height: 30 }}
                   alt="profile"
-                  src=""
+                  src={`https://www.themoviedb.org/t/p/w64_and_h64_face${user?.avatar?.tmdb?.avatar_path}`}
                 />
               </Button>
             )}
